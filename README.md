1fd4f16a901a6704d3e416699c7142fa8e5790c2
clin520 <clin520@126.com>
2018-02-12 15:06:49 +0800

    modify mysql and redis connect

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## 该项目为docker多容器互联方案最后方案
 > --lsky
 
+    1. git clone git@github.com:Lsky1026/multi-container-interconnection.git
+
+    2. docker network create range-net
+
+    3.docker-compose up -d
+
+    4.所有依赖全部安装完成 打开浏览器 输入localhost:8001及看到phpinfo、mysql和redis连接信息
+    !["成功照片"](./success.png)
--- data/index.php
+++ data/index.php
@@ -1,8 +1,15 @@
 <?php
     $conn = new mysqli('lsky_mysql', 'root', '123456');
     if($conn){
-        echo 'success';
+        echo 'Database connection is successful';
+        $conn->close();
     }else{
-        echo 'error';
+        echo 'Unable to connect to the database';
     }
+
+    echo '<br />';
+
+    $redis = new Redis();
+    $redis->connect('lsky_redis', 6379);
+    echo "redis server is running: " . $redis->ping();
 	phpinfo();
